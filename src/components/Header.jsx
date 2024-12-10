6c486f88ad0c3a04a0b0dfadc48ef63f2aeb8e23
Amanuel Belete <amanuelbelete95@gmail.com>
2024-12-10 12:59:32 +0300

    Update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,54 +1,73 @@
 import { Link } from 'react-router-dom';
-import { Box, IconButton, Text } from '@chakra-ui/react';
+import { Box, IconButton, Text, Flex } from '@chakra-ui/react';
 import { BsRecycle } from 'react-icons/bs';
 
 function Header() {
   return (
     <header>
-      <Box
-        display={'flex'}
-        justifyContent={'flex-start'}
-        fontWeight={'900'}
-        fontSize={'x-large'}
-        gap={'40px'}
-        color={'#fff'}
-        alignItems={'center'}
-        height={'100px'}
-        background={'green'}>
-        <Text as={'h1'}>Cleaning Management Agency-Sanitation</Text>
-        <IconButton
-          aria-label='logo'
-          size={'xxlg'}
-          icon={<BsRecycle />}
-        />
-      </Box>
+      {/* Header Top Section */}
       <Box
         display='flex'
-        alignItems={'center'}
-        justifyContent={'center'}
-        gap={'20px'}
-        fontSize={'4xl'}
-        fontWeight={'900'}>
-        <Link
-          to='/'
-          size={'lg'}>
-          Home
-        </Link>
-        <Link
-          to='/work'
-          size={'lg'}>
-          Work
-        </Link>
-        <Link
-          to='/team'
-          size={'lg'}>
-          Team
-        </Link>
-        <Link
-          to='/contact'
-          size={'lg'}>
-          Contacts
-        </Link>
+        justifyContent='space-between'
+        alignItems='center'
+        padding='0 40px'
+        height='100px'
+        background='green'
+        color='white'>
+        {/* Logo and Title */}
+        <Flex
+          alignItems='center'
+          gap='15px'>
+          <IconButton
+            aria-label='logo'
+            icon={<BsRecycle />}
+            size='lg'
+            colorScheme='teal'
+            variant='ghost'
+          />
+          <Text
+            as='h1'
+            fontSize='xl'
+            fontWeight='bold'>
+            Cleaning Management Agency - Sanitation
+          </Text>
+        </Flex>
+      </Box>
+
+      {/* Navigation Section */}
+      <Box
+        display='flex'
+        justifyContent='center'
+        alignItems='center'
+        background='white'
+        padding='10px 0'
+        boxShadow='sm'
+        fontWeight='bold'>
+        <Flex
+          gap='30px'
+          fontSize='lg'
+          color='green'>
+          <Link
+            to='/'
+            style={{ textDecoration: 'none' }}>
+            Home
+          </Link>
+          <Link
+            to='/work'
+            style={{ textDecoration: 'none' }}>
+            Work
+          </Link>
+          <Link
+            to='/team'
+            style={{ textDecoration: 'none' }}>
+            Team
+          </Link>
+          <Link
+            to='/contact'
+            style={{ textDecoration: 'none' }}>
+            Contacts
+          </Link>
+        </Flex>
       </Box>
     </header>
   );
