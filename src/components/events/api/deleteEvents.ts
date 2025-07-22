df94eb6ddab53b43a16d7d362be3fa65262c077a
Amanuel Belete <amanuelbelete95@gmail.com>
2025-07-22 10:25:58 +0300

    Update the table and add dashboard table

--- src/components/events/api/deleteEvents.ts
+++ src/components/events/api/deleteEvents.ts
@@ -1,7 +1,7 @@
 import { BASE_URL } from "../constants";
 
 export const onDelete = async (id: string): Promise<Response> => {
-  const response = await fetch(`${BASE_URL}/api/events/${id}`, {
+  const response = await fetch(`${BASE_URL}/api/events/${id}/delete`, {
     method: "DELETE",
     headers: {
       'Content-Type': 'application/json',
