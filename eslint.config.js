8aa1872f6365c9d2c1056d5ff5423138f3d706e5
Amanuel Belete <amanuelbelete95@gmail.com>
2026-04-08 16:14:46 +0300

    update to vite

--- eslint.config.js
+++ eslint.config.js
@@ -16,7 +16,6 @@ export default [
                 ecmaFeatures: {
                     jsx: true,
                 },
-                project: './tsconfig.json',
             },
         },
         plugins: {
