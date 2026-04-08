41ebf060fc39c96179e77bf87eb1be915d1be217
Amanuel Belete <amanuelbelete95@gmail.com>
2026-04-08 16:32:49 +0300

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,12 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/events/',
+  base: '/', 
   server: {
     port: 3000,
   },
   build: {
-    outDir: 'build',
+    outDir: 'dist', 
     sourcemap: true,
   },
 });
